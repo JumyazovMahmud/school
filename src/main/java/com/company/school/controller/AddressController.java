e1aa32aadeab0e6da6537124effd230353ea997a
Habibullo <sayfullayevhabibullo3366@gmail.com>
2024-02-22 16:53:14 +0500

    Controller complete

--- src/main/java/com/company/school/controller/AddressController.java
+++ src/main/java/com/company/school/controller/AddressController.java
@@ -1,4 +1,38 @@
 package com.company.school.controller;
 
-public class AddressController {
+import com.company.school.dto.ResponseDto;
+import com.company.school.dto.request.RequestAddressDto;
+import com.company.school.dto.response.ResponseAddressDto;
+import com.company.school.util.SCrud;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "address")
+public class AddressController implements SCrud<ResponseAddressDto,Integer, RequestAddressDto> {
+    @Override
+    @PostMapping
+    public ResponseEntity<ResponseDto<ResponseAddressDto>> create(RequestAddressDto request) {
+        return null;
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<ResponseDto<ResponseAddressDto>> get(Integer entityId) {
+        return null;
+    }
+
+    @Override
+    @PutMapping
+    public ResponseEntity<ResponseDto<ResponseAddressDto>> update(Integer entityId, RequestAddressDto request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping
+    public ResponseEntity<ResponseDto<ResponseAddressDto>> delete(Integer entityId) {
+        return null;
+    }
 }
--- src/main/java/com/company/school/controller/RoomsController.java
+++ src/main/java/com/company/school/controller/RoomsController.java
@@ -1,4 +1,38 @@
 package com.company.school.controller;
 
-public class RoomsController {
+import com.company.school.dto.ResponseDto;
+import com.company.school.dto.request.RequestRoomsDto;
+import com.company.school.dto.response.ResponseRoomsDto;
+import com.company.school.util.SCrud;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "rooms")
+public class RoomsController implements SCrud<ResponseRoomsDto,Integer, RequestRoomsDto> {
+    @Override
+    @PostMapping
+    public ResponseEntity<ResponseDto<ResponseRoomsDto>> create(RequestRoomsDto request) {
+        return null;
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<ResponseDto<ResponseRoomsDto>> get(Integer entityId) {
+        return null;
+    }
+
+    @Override
+    @PutMapping
+    public ResponseEntity<ResponseDto<ResponseRoomsDto>> update(Integer entityId, RequestRoomsDto request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping
+    public ResponseEntity<ResponseDto<ResponseRoomsDto>> delete(Integer entityId) {
+        return null;
+    }
 }
