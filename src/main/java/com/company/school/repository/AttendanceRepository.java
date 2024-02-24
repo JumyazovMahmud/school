5854cd5dc4bd9adc432f033b0b0931d3ceeebdee
Fazliddin Tokhirov <fff@MacBook-Pro-FFF.local>
2024-02-24 16:51:47 +0500

    update

--- src/main/java/com/company/school/repository/AttendanceRepository.java
+++ src/main/java/com/company/school/repository/AttendanceRepository.java
@@ -1,11 +1,12 @@
 package com.company.school.repository;
 
 import com.company.school.entity.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AttendanceRepository {
-    Optional<Attendance> findByAddressIdAndDeletedAtIsNull(Integer attendanceId);
+public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
+    Optional<Attendance> findByAttendanceIdAndDeletedAtIsNull(Integer attendanceId);
 }
--- src/main/java/com/company/school/repository/RoomsRepository.java
+++ src/main/java/com/company/school/repository/RoomsRepository.java
@@ -1,12 +1,14 @@
 package com.company.school.repository;
 
+import com.company.school.entity.Parent;
 import com.company.school.entity.Rooms;
 import com.sun.jdi.IntegerValue;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoomsRepository {
-    Optional<Rooms> findByAddressIdAndDeletedAtIsNull(Integer roomsId);
+public interface RoomsRepository extends JpaRepository<Rooms,Integer> {
+    Optional<Rooms> findByRoomsIdAndDeletedAtIsNull(Integer roomsId);
 }
